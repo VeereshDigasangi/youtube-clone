4d32017877427db40bc61690b4b13800cf1b975d
veeresh-dg <veeresh.digasangi@mutualmobile.com>
2023-03-01 20:40:51 +0530

    removed youtube app key

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,4 +1,4 @@
-const YOUTUBE_API_KEY = "AIzaSyAWGr8ChnF83iFGUWWmyL1Hl47V57wPAEk";
+const YOUTUBE_API_KEY = "changeme";
 export const YOUTUBE_VIDEO_LIST_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   YOUTUBE_API_KEY;
